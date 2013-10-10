9c803cd4a290a864f97be5d02adc5636990ff58b
Cyrus Team <cyrusteam@cyruslists.com>
2013-10-10 16:33:34 -0400

    Can switch from Main Activity to the Triangle Activity.

--- AndroidSandbox/src/main/java/com/cyrus/sandbox/MainActivity.java
+++ AndroidSandbox/src/main/java/com/cyrus/sandbox/MainActivity.java
@@ -1,17 +1,32 @@
 package com.cyrus.sandbox;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    private Button triangleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        triangleButton = (Button)findViewById(R.id.triangleButton);
+        triangleButton.setOnClickListener(switchActivity);
     }
 
+    View.OnClickListener switchActivity = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent myIntent1 = new Intent(MainActivity.this, TriangleActivity.class);
+            MainActivity.this.startActivity(myIntent1);
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
